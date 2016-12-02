0d5c9bf397ce880e075110b89b5acaaa13783b7b
niketarachhadia <niketa.rachhadia@gmail.com>
2016-12-02 01:40:29 -0600

    database connection string

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 export default{ DATABASE_URL : process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                       (process.env.NODE_ENV === 'production' ?
-                            'mongodb://test:test@ds119768.mlab.com:19768/worthysum-mlab' :
-                            'mongodb://localhost/worthysum-dev'),
+                       'mongodb://test:test@ds119768.mlab.com:19768/worthysum-mlab',
  PORT : process.env.PORT || 3001
 }

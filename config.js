6d2a52a7485fd75df65d266114191650edf1bcc5
niketarachhadia <niketa.rachhadia@gmail.com>
2016-12-02 00:26:13 -0600

    Mlab mongodb configuraiton

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export default{ DATABASE_URL : process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://test:test@ds047666.mlab.com:47666/xml2json-mlab' :
+                            'mongodb://test:test@ds119768.mlab.com:19768/worthysum-mlab' :
                             'mongodb://localhost/worthysum-dev'),
  PORT : process.env.PORT || 3001
 }

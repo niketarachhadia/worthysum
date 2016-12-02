2d30eb678190794f1fb4cc90fdda8089a5883526
niketarachhadia <niketa.rachhadia@gmail.com>
2016-12-02 01:44:35 -0600

    database connection string

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-export default{ DATABASE_URL : process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                       'mongodb://test:test@ds119768.mlab.com:19768/worthysum-mlab',
+export default{ DATABASE_URL : 'mongodb://test:test@ds119768.mlab.com:19768/worthysum-mlab',
  PORT : process.env.PORT || 3001
 }
